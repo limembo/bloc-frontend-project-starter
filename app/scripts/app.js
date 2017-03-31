19b207fc888fc3d80247cd2731af9bcec7c9aeee
danielleoverman <danielleoverman1@gmail.com>
2017-03-30 22:31:55 -0400

    Checkpoint 2 Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,26 @@
-(function(){ 
+
+(function() {
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-            enabled: true,
-            requireBase: false
-         });
-
-     $stateProvider
-         .state('index', {
-             url: '/',
-             templateUrl: '/index.html'
-         });
- 
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeCtrl as home',
+                templateUrl: '/templates/home.html'
+            })
+            .state('room', {
+                url: '/room',
+                controller: 'RoomCtrl as room',
+                templateUrl: '/templates/room.html'
+            });
     }
+    
     angular
-         .module('blocChat', ['ui.router', 'firebase'])
-         .config(config);
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
 })();
-
